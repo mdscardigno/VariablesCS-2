dd1aa2262732ea190521c9aeb2e8091adde1ed18
Milagro Scardigno <mdscardigno@gmail.com>
2022-08-17 19:12:40 -0400

    calculations with floats and decimals

--- Program.cs
+++ Program.cs
@@ -17,11 +17,21 @@ namespace VariablesCS
             var name = Console.ReadLine();
 
             Console.Write("It is a pleasure to meet you, ");
-            Console.Write(name);
+            Console.Write("\n" + name);
 
             int score1 = 100;
             int score2 = score1 + 1;
-            Console.WriteLine(score1 + score2);
+            Console.WriteLine("\n" + (score1 + score2));
+
+            var float1 = 1.0f;
+            var float2 = 2.0f;
+            Console.WriteLine(float1 += float2);
+            var dec3 = 0.33m;
+            var dec4 = 0.81m;
+            Console.WriteLine(dec3 + dec4);
+            var dec5 = 0.33m;
+            var dec6 = 0.84m;
+            Console.WriteLine(dec5 + dec6);
         }
     }
 }
