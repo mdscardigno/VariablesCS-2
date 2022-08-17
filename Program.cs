6840fc1c1a40b7deee31c8eba7a7b3fc665394a7
Milagro Scardigno <mdscardigno@gmail.com>
2022-08-17 19:03:08 -0400

    variables exercise

--- Program.cs
+++ Program.cs
@@ -2,11 +2,26 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Practice Creating Variables");
+            int numberOfCupsOfCoffee = 2;
+            string fullName = "John Doe";
+            DateTime today = DateTime.Now;
+
+            Console.WriteLine("My name is " + fullName + ". " + "Today I drank " + numberOfCupsOfCoffee + " cups of coffee. Today is " + today);
+
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+
+            Console.Write("It is a pleasure to meet you, ");
+            Console.Write(name);
+
+            int score1 = 100;
+            int score2 = score1 + 1;
+            Console.WriteLine(score1 + score2);
+        }
     }
-  }
 }
